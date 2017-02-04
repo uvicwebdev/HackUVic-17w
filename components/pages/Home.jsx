925a37b67064b32ad3cb7931babdaaa4acb0bee1
Brynn Hawker <brynn@hawker.me>
2017-02-04 14:37:24 -0800

    About section

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -7,6 +7,8 @@ import Icon from 'parts/Icon';
 import Logo from 'img/Logos/Logo_Type_Light.svg';
 import Stars from 'img/Stars.svg';
 
+import About from 'sections/About';
+
 export default class Home extends Component {
   // static propTypes = {
   // }
@@ -26,31 +28,30 @@ export default class Home extends Component {
 
   render() {
     return (
-      <main className="home" style={{ backgroundImage: `url(${Stars})` }}>
-        <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2017/events?utm_source=na-2017&utm_medium=TrustBadge&utm_campaign=na-2017&utm_content=white" target="_blank" rel="noopener noreferrer">
-          <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg" alt="Major League Hacking 2017 Hackathon Season" />
-        </a>
-        <div className="hero">
-          <div>
-            <h1>
-              <img src={Logo} alt="HackUVic Logo" />
-              <br /><br />
-              <small>
-                March 14th and 15th at the University of Victoria
-              </small>
-            </h1>
-            <br />
-            <button className="btn btn-hero btn-fat">
-              <Icon name="ticket" size={1.3} /> Register in 30 seconds
-            </button>
+      <div className="home">
+        <main className="content" style={{ backgroundImage: `url(${Stars})` }}>
+          <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2017/events?utm_source=na-2017&utm_medium=TrustBadge&utm_campaign=na-2017&utm_content=white" target="_blank" rel="noopener noreferrer">
+            <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg" alt="Major League Hacking 2017 Hackathon Season" />
+          </a>
+          <div className="hero">
+            <div>
+              <h1>
+                <img src={Logo} alt="HackUVic Logo" />
+                <br /><br />
+                <small>
+                  March 11th and 12th at the University of Victoria
+                </small>
+              </h1>
+              <br />
+              <button className="btn btn-hero btn-fat">
+                <Icon name="ticket" size={1.3} /> Register in 30 seconds
+              </button>
+            </div>
           </div>
-        </div>
-        <section>
-          <div className="container">
-            <h2>Guidelines</h2>
-          </div>
-        </section>
-      </main>
+          {/* Sections */}
+          <About />
+        </main>
+      </div>
     );
   }
 }
