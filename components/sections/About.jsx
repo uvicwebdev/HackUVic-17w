62ea82d168b5fc9cadf176d40c6095dfdbe8622e
Brynn Hawker <brynn@hawker.me>
2017-02-26 23:09:23 -0800

    Fix quote

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -20,7 +20,7 @@ export default class About extends Component {
               <h2>What is HackUVic?</h2>
             </div>
             <div className="column col-sm-12 col-6">
-              <p>HackUVic is a hackathon hosted by the Uvic Web Design and Development Club at the University of Victoria. It&quot;s an event for developers and designers of all skill levels to build their own project from scratch.</p>
+              <p>HackUVic is a hackathon hosted by the Uvic Web Design and Development Club at the University of Victoria. It's an event for developers and designers of all skill levels to build their own project from scratch.</p>
               <p>The event is free and food will be provided. Prizes for winners. Recommended team size is 2 - 5, we'll pair you up other students attending the event if you can't find a team.</p>
             </div>
             <div className="column col-sm-12 col-6">
